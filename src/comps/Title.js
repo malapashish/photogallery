3e06293feef735cd5dc5f31ed9bb6c54d254f206
malapashish <malapashish10@gmail.com>
2021-04-17 16:53:44 +0530

    Initial commit

---
+++ src/comps/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>PhotoGallery</h1> 
+      {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/storage';  //tostore our images
+import 'firebase/firestore';  //database
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDojtLgbOlNGsXXQQffK2HdOzalbhDJqkk",
+    authDomain: "ash-photogallary.firebaseapp.com",
+    projectId: "ash-photogallary",
+    storageBucket: "ash-photogallary.appspot.com",
+    messagingSenderId: "186175180113",
+    appId: "1:186175180113:web:e7f4d3e5defbed4ca1d770"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig); 
+
+const projectStorage = firebase.storage();
+const projectFireStore = firebase.firestore();
+const timeStamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export {projectStorage , projectFireStore , timeStamp };
